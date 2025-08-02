64a901aef89ee6196b365c4f450c335ddc5b1c03
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-08-02 15:49:18 +0530

    Train the AI model

---
+++ Training_model/model.py
@@ -0,0 +1,53 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Head.Speak import speak
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from Head.Speak import speak
+
+# Load your Q&A data set from a text file
+def load_dataset(file_path):
+    with open(file_path,'r',encoding='utf-8') as file:
+        lines = file.readlines()
+        qna_pairs = [line.strip().split(':') for line in lines if ':' in line]
+        dataset = [{'question':q,'answer':a} for q,a in qna_pairs]
+    return dataset
+
+# Preprocess the text
+def preprocess_text(text):
+    stop_words = set(stopwords.words('english'))
+    ps = PorterStemmer()
+    tokens = word_tokenize(text.lower())
+    tokens = [ps.stem(token) for token in tokens if token.isalnum() and token not in stop_words]
+    return ' '.join(tokens)
+
+# Train the TF-IDF vectorizer
+def train_tfidf_vectorizer(dataset):
+    corpus = [preprocess_text(qa['question']) for qa in dataset]
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    return vectorizer, X
+
+# Retrieve the most relevant answer
+def get_answer(question, vectorizer, X,dataset):
+    question = preprocess_text(question)
+    question_vec = vectorizer.transform([question])
+    similarities = cosine_similarity(question_vec, X)
+    best_match_index = similarities.argmax()
+    return dataset[best_match_index]['answer']
+
+# Main Function
+def mind(text):
+    dataset_path = r'C:\Users\ratho\OneDrive\Desktop\ZERO_2.0\Data\Brain_data\qna_data.txt'
+    dataset = load_dataset(dataset_path)
+
+    vectorizer, X = train_tfidf_vectorizer(dataset)
+    user_question = text
+    answer = get_answer(user_question, vectorizer, X, dataset)
+    speak(answer)
